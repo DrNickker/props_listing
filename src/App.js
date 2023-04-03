490e7344c2cb99ea67de0195cb32d1a864d7409b
DrNickker <mintagirov2015@yandex.ru>
2023-04-03 15:27:07 +0500

    2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './main.css';
+import Listing from './components/listing';
+import {etsy} from './components/data'
 
 function App() {
+  const arr = []
+  etsy.map(el => {
+    const {listing_id, url, MainImage, title, currency_code, price, quantity, category_id} = el
+    return arr.push({listing_id, url, MainImage, title, currency_code, price, quantity, category_id});
+  })
+
+  console.log(arr)
+  const data = arr.filter(el => el.MainImage !== undefined )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <React.Fragment>
+      <div className="item-list"> 
+        <Listing data={data} />
+      </div>
+    </React.Fragment>
+  )
 }
 
 export default App;
